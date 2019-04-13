8a1e272151e1185d0a486da47822f40ac0be3831
Daniel A. Nagy <nagy.da@gmail.com>
2019-04-14 00:24:13 +0200

    Modification date updated.

--- zx82.asm
+++ zx82.asm
@@ -49,7 +49,7 @@
 ; See http://www.worldofspectrum.org/permits/amstrad-roms.txt for details.
 
 ; -------------------------
-; Last updated: 10-APR-2019
+; Last updated: 13-APR-2019
 ; -------------------------
 
 ; Notes on labels: Entry points whose location is exactly the same as it was
